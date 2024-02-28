eae3725cff7f55e931c2800fe5f0f2a96807a3b6
Zecheng <johnsonchow@chouworld.cc>
2024-02-28 15:22:59 +0800

    Site updated: 2024-02-28 15:22:59

---
+++ gallery/tools.js
@@ -0,0 +1,193 @@
+let json = [{
+        "title": "2020年的开端",
+        "full_link": "https://test.img.hoski.cn/picTop10In2020_1.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop10In2020_1.jpg",
+        "descr": "2020年1月22日，疫情封城前的那2天。突然有种恐慌感的平静。总之夕阳美好，一路稳定，也是东海航空第一次的体验，作为小飞友的祝福，希望民营民航越来越好。"
+    },
+    {
+        "title": "2020年在名古屋之旅的我",
+        "full_link": "https://test.img.hoski.cn/picTop11In2020_2.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop11In2020_2.jpg",
+        "descr": "2020年1月11日，拍摄于名古屋城，但是由于当时流行小怪兽手绘，所以自己尝试了一波。当时疫情其实已经爆发了，名古屋入境有提示武汉-新加坡往返的那趟飞机的乘客需要入境特殊申报。但是这趟也是非常值得的，因为99.99%的人在2020年都没有一趟国际旅行，也是我第一次日本中部的旅行。"
+    },
+    {
+        "title": "2020年带公司小伙伴海南之行",
+        "full_link": "https://test.img.hoski.cn/picTop12In2020_3.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop12In2020_3.jpg",
+        "descr": "2020年5月16日，海南自驾车上拍的。"
+    },
+    {
+        "title": "2020年的货斯基研发部初创成员的一部分",
+        "full_link": "https://test.img.hoski.cn/picTop13In2020_4.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop13In2020_4.jpg",
+        "descr": "2020年5月16日，海滩日落的剪影。"
+    },
+    {
+        "title": "2020年亚龙湾的自然公园",
+        "full_link": "https://test.img.hoski.cn/picTop14In2020_5.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop14In2020_5.jpg",
+        "descr": "2020年5月17日，有一种夏威夷即视感。"
+    },
+    {
+        "title": "2020年三亚返回南京的航班",
+        "full_link": "https://test.img.hoski.cn/picTop15In2020_6.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop15In2020_6.jpg",
+        "descr": "浅蓝机内灯光和夕阳的一种高级配色。"
+    },
+    {
+        "title": "一个夏日暴雨之后的南京五桥",
+        "full_link": "https://test.img.hoski.cn/picTop16In2020_7.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop16In2020_7.jpg",
+        "descr": "那个时候应该是南京五桥才开放不久，对于我来说有多了个方便的过江通道。"
+    },
+    {
+        "title": "氛围感十足的鸭子船",
+        "full_link": "https://test.img.hoski.cn/picTop17In2020_8.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop17In2020_8.jpg",
+        "descr": "2020年8月2日，夕阳西下的百家湖。自己比较喜欢的高级感。"
+    },
+    {
+        "title": "第一次200kg",
+        "full_link": "https://test.img.hoski.cn/picTop18In2020_9.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop18In2020_9.jpg",
+        "descr": "2020年8月28日的第一次深蹲200kg。"
+    },
+    {
+        "title": "2020年的年尾一场雪",
+        "full_link": "https://test.img.hoski.cn/picTop19In2020_10.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop19In2020_10.jpg",
+        "descr": "一种极致的清爽感。"
+    },
+    {
+        "title": "2021年2月9日的平静感",
+        "full_link": "https://test.img.hoski.cn/picTop20In2021_1.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop20In2021_1.jpg",
+        "descr": "一切非常平静，路过百家湖的桥段，觉得天气非常干净，并且夕阳的位置起到好处"
+    },
+    {
+        "title": "第一次用减光镜的作品",
+        "full_link": "https://test.img.hoski.cn/picTop21In2021_2.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop21In2021_2.jpg",
+        "descr": "2021年的清明大理丽江出游，经过的玉龙雪山的蓝月湖。"
+    },
+    {
+        "title": "即将降落丽江机场用大光圈的作品1",
+        "full_link": "https://test.img.hoski.cn/picTop22In2021_3.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop22In2021_3.jpg",
+        "descr": "2021年的清明大理丽江出游，天津航空即将降落丽江机场的傍晚，有点失焦估计是飞机下降速度大且快门速度慢的原因，但是有一种不错的氛围感。"
+    },
+    {
+        "title": "即将降落丽江机场用大光圈的作品2",
+        "full_link": "https://test.img.hoski.cn/picTop23In2021_4.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop23In2021_4.jpg",
+        "descr": "对比上一张，焦点找回来了，哈哈哈。"
+    },
+    {
+        "title": "玉龙雪山摄影级作品",
+        "full_link": "https://test.img.hoski.cn/picTop24In2021_5.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop24In2021_5.jpg",
+        "descr": "拍摄于白沙古镇大门门口的路上。"
+    },
+    {
+        "title": "玉龙雪山的缆车上",
+        "full_link": "https://test.img.hoski.cn/picTop25In2021_6.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop25In2021_6.jpg",
+        "descr": "除了吸氧估计就是拍拍拍了。"
+    },
+    {
+        "title": "丽江古镇旁边咖啡店内的我",
+        "full_link": "https://test.img.hoski.cn/picTop26In2021_7.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop26In2021_7.jpg",
+        "descr": "这家店真不错，出品非常棒，名字叫做屿幸"
+    },
+    {
+        "title": "疫情下的南京傍晚",
+        "full_link": "https://test.img.hoski.cn/picTop27In2021_8.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop27In2021_8.jpg",
+        "descr": "才开通的吉印大道桥的自行车道拍的傍晚，适合一家人来散步。"
+    },
+    {
+        "title": "威海的海鸥",
+        "full_link": "https://test.img.hoski.cn/picTop28In2021_9.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop28In2021_9.jpg",
+        "descr": "本次出行带外公外婆来威海玩玩，顺便自己也散散心，因为考学压力非常大。"
+    },
+    {
+        "title": "2021年十一黄金周的工作外派旅行",
+        "full_link": "https://test.img.hoski.cn/picTop29In2021_10.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop29In2021_10.jpg",
+        "descr": "研发部集体出门为了十一完成深圳某公司的车辆调度项目，项目交付后带小伙伴们去惠州双月湾玩玩，这是他们一家不错的咖啡店，非常推荐。"
+    },
+    {
+        "title": "A7C的航模摄影作品",
+        "full_link": "https://test.img.hoski.cn/picTop30In2022_1.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop30In2022_1.jpg",
+        "descr": "配上了一个小廊桥，让5APU更有地位。"
+    },
+    {
+        "title": "第一次有记忆的爬紫金山",
+        "full_link": "https://test.img.hoski.cn/picTop31In2022_2.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop31In2022_2.jpg",
+        "descr": " 和夏夏爬紫金山登顶，看到南京城的朦胧剪影。"
+    },
+    {
+        "title": "第一次30+聚会",
+        "full_link": "https://test.img.hoski.cn/picTop32In2022_3.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop32In2022_3.jpg",
+        "descr": "2022年南京疫情后的清明假期，大家都在放飞自我。这是跟霞姐，波哥和CECE的一场30+聚会，虽然我没有30+哈哈哈。"
+    },
+    {
+        "title": "大校场隧道的光影作品",
+        "full_link": "https://test.img.hoski.cn/picTop33In2022_4.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop33In2022_4.jpg",
+        "descr": "大校场隧道的光影作品"
+    },
+    {
+        "title": "第一次国内商务舱的纪念",
+        "full_link": "https://test.img.hoski.cn/picTop34In2022_5.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop34In2022_5.jpg",
+        "descr": "厦门航空，南京到厦门，商务舱仅仅800+，感叹厦门航空的餐食真的是不错，尤其是松茸汤好评。"
+    },
+    {
+        "title": "商务舱的小巴接送看到的厦门太古修理厂",
+        "full_link": "https://test.img.hoski.cn/picTop35In2022_6.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop35In2022_6.jpg",
+        "descr": "近距离看787的伟岸身姿。"
+    },
+    {
+        "title": "厦门非常不错的意大利菜",
+        "full_link": "https://test.img.hoski.cn/picTop36In2022_7.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop36In2022_7.jpg",
+        "descr": "最爱的烤蔬菜，但是这家店目前倒闭了。"
+    },
+    {
+        "title": "29岁的生日当天，非常值得纪念",
+        "full_link": "https://test.img.hoski.cn/picTop37In2022_8.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop37In2022_8.jpg",
+        "descr": "29岁生日当天，冲破最大深蹲重量，并且也拿到了USYD的offer简直是多喜临门。"
+    },
+    {
+        "title": " 南京的火烧云",
+        "full_link": "https://test.img.hoski.cn/picTop38In2022_9.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop38In2022_9.jpg",
+        "descr": "下班晚高峰的江北快速路"
+    },
+    {
+        "title": "南京火烧云的背面",
+        "full_link": "https://test.img.hoski.cn/picTop39In2022_10.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop39In2022_10.jpg",
+        "descr": "基金的台风云，清冷不失一份寂静和内劲"
+    },
+    {
+        "title": "第一次深秋，不是，初冬的露营",
+        "full_link": "https://test.img.hoski.cn/picTop40In2022_11.jpg",
+        "thumb_link": "https://test.img.hoski.cn/picTop40In2022_11.jpg",
+        "descr": "2022年年底露营，结果冷空气太快，在第一天冷的时候就被我们遇上了。最后玩了2个小时小雨提前回家。"
+    }
+]
+json.forEach((element,index) => {
+    console.log(element.title+':')
+    console.log("  full_link:"+element.full_link)
+    console.log("  thumb_link:"+element.thumb_link)
+    console.log("  descr:"+element.descr)
+});
